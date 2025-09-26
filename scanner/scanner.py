fdf392899142ca63583b0c9c3a676e3e8e288b65
vinokura <vinokura.kura@gmail.com>
2025-09-27 02:00:28 +0300

    edit scanner

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,7 +1,7 @@
 import socket
 
 def check_port(ip: str, port: int) -> bool:
-# Проверка открытых портов
+    """Проверка открытых портов"""
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(2)
@@ -11,29 +11,16 @@ def check_port(ip: str, port: int) -> bool:
     except:
         return False
 
-
-def main():
-# Сканирование портов
-    ip = input("Введите IP-адрес для сканирования: ").strip()
-
+def get_open_ports(ip: str) -> list:
+    """Создает переменную с открытыми портами IP"""
     ports = [21, 22, 23, 80, 443, 3389, 5900]
-
+    
 #    print(f"Сканируем {ip}...")
-
-# Сканируем и сохраняем открытые порты
+    
     open_ports = []
-
+    
     for port in ports:
         if check_port(ip, port):
             open_ports.append(port)
-            print(f"Порт {port} открыт")
-#       else:
-#           print(f"Порт {port} закрыт")
-
-    #print(f"\n Открытые порты: {open_ports}")
-
+    
     return open_ports
-
-
-if __name__ == "__main__":
-    open_ports = main()
