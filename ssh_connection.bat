ba3414a74a9b7ac0e412ceff84c048939e652398
DemonWolf02 <dimonwolf02@gmail.com>
2025-09-27 05:30:45 +0300

    Delete

--- ssh_connection.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 > nul
-echo Запуск SSH подключения...
-echo Логин: root@91.217.76.107
-echo.
-ssh root@91.217.76.107
-echo.
-echo Сессия завершена. Нажмите любую клавишу...
-pause
